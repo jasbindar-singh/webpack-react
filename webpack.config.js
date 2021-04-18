b2b3248c809b012b1add7f978e2a615ce6ad103b
Jasbindar Singh <jasbindar1998@gmail.com>
2021-04-18 19:39:57 +0530

    [Config] Added bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,18 @@ const commonConfig = require("./webpack.common")();
 
 const getEnvConfig = (mode) => require(`./webpack.${mode}`)();
 
+const getPresetConfig = (preset) => {
+  if (!preset) return {};
+  else return require("./webpack.analyze")();
+};
+
 module.exports = (env) => {
   return merge(
     {
       mode: env.mode,
     },
     commonConfig,
-    getEnvConfig(env.mode)
+    getEnvConfig(env.mode),
+    getPresetConfig(env.preset)
   );
 };
